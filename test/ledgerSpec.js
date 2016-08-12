c702d44e5f3b39efa09705a92f63caebd4522c2c
Ben Sharafian <benms64@gmail.com>
2016-08-12 16:15:33 -0700

    test: add more tests

--- test/ledgerSpec.js
+++ test/ledgerSpec.js
@@ -39,4 +39,48 @@ contract('Ledger', function (accounts) {
         })
     })
   })
+
+  describe('fulfillTransfer', function () {
+    beforeEach(function () {
+      this.fulfillment = '0x0'
+      this.ledger = Ledger.deployed()
+      this.opts = [
+        accounts[0],
+        '0xb8ad1bd2ff50021ff6a1239585cc9ccde31e70072299c3cc910da54f9e791f7c',
+        '0x1',
+        '0x0',
+        '',
+      ]
+    })
+
+    it('should expire an expired transfer', function () {
+      this.ledger.createTransfer(...this.opts)
+        .then(() => {
+          return this.ledger.fulfillTransfer.call('0x1', '0x0')
+        })
+        .then((result) => {
+          assert.equal(result.valueOf(), 1, 'expired transfer should return 1 on fulfill')
+        })
+    })
+
+    it('should give an error code fulfilling a finished transfer', function () {
+      this.ledger.createTransfer(...this.opts)
+        .then(() => {
+          return this.ledger.fulfillTransfer('0x1', '0x0')
+        })
+        .then(() => {
+          return this.ledger.fulfillTransfer.call('0x1', '0x0')
+        })
+        .then((result) => {
+          assert.equal(result.valueOf(), -2, 'double fulfill should return -2')
+        })
+    })
+
+    it('should give an error code fulfilling a nonexistant transfer', function () {
+      this.ledger.fulfillTransfer.call('0x1', '0x0')
+        .then(() => {
+          assert.equal(result.valueOf(), -1, 'nonexistant transfer should return -1 on fulfill')
+        })
+    })
+  })
 })
