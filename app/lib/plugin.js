164de4d47eada01d770e39182e5276df0d82c0b2
Ben Sharafian <benms64@gmail.com>
2017-02-15 16:11:56 +0100

    add fulfill event

--- app/lib/plugin.js
+++ app/lib/plugin.js
@@ -66,6 +66,46 @@ class PluginEthereum extends EventEmitter {
       console.log('debug message (int):', result.args.msg, result.args.num)
     })
 
+    this.contract.Fulfill((error, result) => {
+
+      try {
+//        console.log('EVENT:', result.args)
+        const uuid = result.args.uuid
+        const state = 1
+        const fulfillment = result.args.fulfillment
+ //       console.log('uuid, state:', uuid, state)
+
+        this.contract.transfers(uuid, (err, result) => {
+          const res = result.map((e) => e.toString())
+  //        console.log('\x1b[31mGot Event Result:', res)
+          const memo = this.contract.memos(uuid, (err, result) => {
+            let data
+            try {
+              data = JSON.parse(Buffer.from(result.slice(2), 'hex').toString('utf8'))
+            } catch (e) {
+              data = {}
+            }
+            console.log('\x1b[32mGot Memo:',
+              JSON.parse(Buffer.from(result.slice(2), 'hex').toString('utf8') || '{}'))
+  
+            // parse the event and emit that
+            this._processUpdate({
+              id: uuid,
+              from: this._toAccount(res[0]),
+              to: this._toAccount(res[1]),
+              amount: this.web3.fromWei(res[2]),
+              data: data,
+              executionCondition: 'cc:0:3:' + base64url(Buffer.from(res[3].slice(2), 'hex')) + ':32',
+              noteToSelf: JSON.parse(this.notesToSelf[uuid] || null),
+              expiresAt: (new Date(+res[4] * 1000)).toISOString(),
+              state: 'fulfill',
+            }, 'cf:0:' + base64url(Buffer.from(fufillment.slice(2), 'hex')))
+          })
+        })
+
+      } catch (e) {}
+    })
+
     // listen for the events
     this.contract.Update((error, result) => {
       if (!this.web3) return
@@ -149,6 +189,7 @@ class PluginEthereum extends EventEmitter {
   fulfillCondition (transferId, fulfillment) {
     const uuid = '0x' + transferId.replace(/\-/g, '')
     const fulfillmentBytes = '0x' + Buffer.from(fulfillment.match(/cf:0:(.+)/)[1], 'base64').toString('hex')
+    console.log('fulfillmentBytes:', fulfillmentBytes)
     
     return new Promise((resolve, reject) => {
       const handle = (error, result) => {
